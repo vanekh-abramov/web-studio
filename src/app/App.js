68f24ce11d82674f9430e8d8b4ced6fb4e8e27cc
Vania <vanekh.abramov@gmail.com>
2024-02-27 20:54:28 +0300

    feat: add fix in App

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,9 @@
 import style from './App.module.scss';
-// import { Header } from "../header/Header";
-// import { Home } from "../../pages/home/Home";
-// import { AboutUs } from "../../pages/aboutUs/AboutUs";
 import Header from '../components/header/Header';
 import AppRouter from '../routes/AppRouter';
 
-function App() {
+const App = () => {
+
     return (
         <div className={style.wrapper}>
             <Header />
